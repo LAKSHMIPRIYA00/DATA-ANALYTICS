51b421cac29f0a90b12c18a59e0e6a57d1824581
lakshmi <soo280902@gmail.com>
2023-08-01 18:36:56 +0530

    functions.ipynb

---
+++ polygon.py
@@ -0,0 +1,29 @@
+from turtle import *
+# define a function
+def hexagon():
+    for i in range(6):
+        fd(100)
+        lt(360/6)
+
+
+
+#call the function
+hexagon() 
+penup()  
+goto(100,100)     
+hexagon()   
+pendown()
+goto(-100,100)     
+hexagon()   
+penup()
+goto(-100,-100)     
+hexagon() 
+pendown()  
+goto(100,-100)     
+hexagon() 
+penup()  
+goto(100,100)   
+pendown()
+hexagon()
+hideturtle()
+mainloop()  
